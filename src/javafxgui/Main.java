504d55afc2e5078fe9f1eeb11afc48a84d4f59a7
Dallan Freemantle <freemadall@ecs.vuw.ac.nz>
2017-05-23 15:33:18 +1200

    Started making JavaFX Gui

---
+++ src/javafxgui/Main.java
@@ -0,0 +1,32 @@
+package javafxgui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	Stage window;
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		window = primaryStage;
+		window.setTitle("AES Cipher");
+		
+		Button button = new Button("OK");
+		
+		BorderPane layout = new BorderPane();
+		layout.setLeft(button);
+		
+		Scene scene = new Scene(layout, 300, 350);
+		window.setScene(scene);
+		window.show();
+	}
+
+}
