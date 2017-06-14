0216cbc31e3be3e5f2f992e3adac7100ec1224f6
Dallan Freemantle <freemadall@ecs.vuw.ac.nz>
2017-06-14 19:57:02 +1200

    Updated progress indicators.

--- src/javafxgui/TestingProgressIndicators.java
+++ src/javafxgui/TestingProgressIndicators.java
@@ -2,6 +2,10 @@ package javafxgui;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.net.URISyntaxException;
@@ -16,8 +20,24 @@ public class TestingProgressIndicators extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        ScrollPane scrollPane = new ScrollPane();
+        GridPane layout = new GridPane();
+        Label fileTitle1 = new Label("File.txt");
+        ProgressIndicator progressIndicator1 = new ProgressIndicator(0.3);
+        Label fileTitle2 = new Label("File.txt");
+        ProgressIndicator progressIndicator2 = new ProgressIndicator(0.3);
+        Label fileTitle3 = new Label("File.txt");
+        ProgressIndicator progressIndicator3 = new ProgressIndicator(0.3);
+        Label fileTitle4 = new Label("File.txt");
+        ProgressIndicator progressIndicator4 = new ProgressIndicator(0.3);
+        layout.addRow(0, progressIndicator1, fileTitle1);
+        layout.addRow(1, progressIndicator2, fileTitle2);
+        layout.addRow(2, progressIndicator3, fileTitle3);
+        layout.addRow(3, progressIndicator4, fileTitle4);
+        scrollPane.setContent(layout);
+
         // Setup window
-        Scene scene = new Scene(null);
+        Scene scene = new Scene(scrollPane);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
